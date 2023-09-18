8e53fc2927929974ffb0d4323899854953c9f6a8
Patryk Ruczkin <pdruczkin@gmail.com>
2023-09-18 19:26:02 +0200

    Add Change UserDetails functionality

--- Api/Controllers/UserDetailsController.cs
+++ Api/Controllers/UserDetailsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.UserDetails.Commands.ChangePassword;
+using Application.UserDetails.Commands.ChangeUserDetails;
 using Application.UserDetails.Queries.GetUserDetails;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,22 +9,27 @@ namespace Api.Controllers;
 public class UserDetailsController : ApiControllerBase
 {
     [HttpPut("change-password")]
-    [Authorize]
     public async Task<ActionResult> ChangePassword([FromBody] ChangePasswordCommand command)
     {
         await Mediator.Send(command);
 
         return Ok();
     }
-
-
+    
     [HttpGet]
-    [Authorize]
     public async Task<ActionResult> GetUserDetails()
     {
         var detailsDto = await Mediator.Send(new GetUserDetailsCommand());
 
         return Ok(detailsDto);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> ChangeUserDetails([FromBody] ChangeUserDetailsCommand command)
+    {
+        await Mediator.Send(command);
+        
+        return Ok();
     }
     
 }
