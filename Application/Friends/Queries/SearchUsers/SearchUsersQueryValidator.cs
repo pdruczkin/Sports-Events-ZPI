c554c39a8817f7461221d40cfa58fabe9abdab96
Marek Schubert <m.schubert@op.pl>
2023-11-01 07:44:15 +0100

    Added SearchPhrase validation

---
+++ Application/Friends/Queries/SearchUsers/SearchUsersQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Friends.Queries.SearchUsers;
+
+public class SearchUsersQueryValidator : AbstractValidator<SearchUsersQuery>
+{
+	public SearchUsersQueryValidator()
+	{
+		RuleFor(x => x.SearchPhrase)
+			.NotEmpty();
+	}
+}
