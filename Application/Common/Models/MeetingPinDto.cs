48b34f30915e5287868dab65420fa64b3b79aca2
NeverGiveUPek <pdruczkin@gmail.com>
2023-11-10 09:20:05 +0100

    Add Organizer Username to User/Friend Details

--- Application/Common/Models/MeetingPinDto.cs
+++ Application/Common/Models/MeetingPinDto.cs
@@ -1,4 +1,7 @@
-﻿using Application.Common.Mappings;
+﻿using Application.Common.ExtensionMethods;
+using Application.Common.Mappings;
+using Application.Friends.Queries.GetFriendDetails;
+using AutoMapper;
 using Domain.Entities;
 using Domain.Enums;
 
@@ -7,10 +10,17 @@ namespace Application.Common.Models;
 public class MeetingPinDto : IMappable<Meeting>
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = "";
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public DateTime StartDateTimeUtc { get; set; }
     public DateTime EndDateTimeUtc { get; set; }
     public SportsDiscipline SportsDiscipline { get; set; }
+    public string OrganizerUsername { get; set; } = "";
+    
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Meeting, MeetingPinDto>()
+            .ForMember(x => x.OrganizerUsername, o => o.MapFrom(s => s.Organizer.Username));
+    }
 }
