641641c4de7928c428354af1a96c2c7aee26e273
Marek Schubert <m.schubert@op.pl>
2023-11-10 08:51:31 +0100

    Search user fix

--- Application/Friends/Queries/SearchUsers/SearchUsersQuery.cs
+++ Application/Friends/Queries/SearchUsers/SearchUsersQuery.cs
@@ -15,17 +15,22 @@ public class SearchUsersQueryHandler : IRequestHandler<SearchUsersQuery, List<Us
 {
     private readonly IApplicationDbContext _applicationDbContext;
     private readonly IMapper _mapper;
+    private readonly IUserContextService _userContextService;
 
-    public SearchUsersQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+    public SearchUsersQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper, IUserContextService userContextService)
     {
         _applicationDbContext = applicationDbContext;
         _mapper = mapper;
+        _userContextService = userContextService;
     }
     public async Task<List<UserIdentityDto>> Handle(SearchUsersQuery request, CancellationToken cancellationToken)
     {
+        var userId = _userContextService.GetUserId;
+
         var users = await _applicationDbContext
             .Users
             .Where(x => x.Username.ToLower().Contains(request.SearchPhrase.ToLower()))
+            .Where(x => x.Id != userId)
             .Take(20)
             .ToListAsync();
 
