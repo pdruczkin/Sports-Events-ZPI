baf1f897796a50906f215b26956b54e564f5e1f5
Marek Schubert <260369@student.pwr.edu.pl>
2023-11-14 15:44:14 +0000

    Revert "Pre-merge fixes"

--- Infrastructure/ConfigureServices.cs
+++ Infrastructure/ConfigureServices.cs
@@ -1,9 +1,7 @@
 ﻿using Application.Common.Interfaces;
 using Application.Common.Models;
-using Hangfire;
 using Infrastructure.Persistence;
 using Infrastructure.Services;
-using Infrastructure.Triggers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,38 +16,25 @@ public static class ConfigureServices
         configuration.GetSection("EmailSender").Bind(emailSenderSettings);
         services.AddSingleton(emailSenderSettings);
 
-        services.AddHangfire(config => config
-            .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-            .UseSimpleAssemblyNameTypeSerializer()
-            .UseRecommendedSerializerSettings()
-            .UseSqlServerStorage(configuration.GetConnectionString("HangfireConnection")));
-
-        services.AddHangfireServer();
-
-        services.AddDbContext<ApplicationDbContext>(options => {
-            options.UseSqlServer(configuration.GetConnectionString("DbConnection"),
-                builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
-
-            options.UseTriggers(triggerOptions => {
-                triggerOptions.AddTrigger<SendEmailTestTrigger>();
-                triggerOptions.AddTrigger<OrgnAchievementTrigger>();
-                triggerOptions.AddTrigger<FrieAchievementTrigger>();
-                triggerOptions.AddTrigger<TimePartAchievementTrigger>();
-            });
-        });
+        var cloudinarySettings = new CloudinarySettings();
+        configuration.GetSection("CloudinarySettings").Bind(cloudinarySettings);
+        services.AddSingleton(cloudinarySettings);
+        
+        
+        services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DbConnection"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        
 
         services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         services.AddScoped<ApplicationDbContextInitializer>();
 
         services.AddHttpContextAccessor();
+        
         services.AddScoped<IUserContextService, UserContextService>();
-
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
-
-        
-        
-        
         services.AddScoped<IEmailSender, EmailSender>();
+        services.AddScoped<IImageService, ImageService>();
         
         return services;
     }
