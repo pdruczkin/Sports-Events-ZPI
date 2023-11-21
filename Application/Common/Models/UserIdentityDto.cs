98a9d03fbbf15b46d3b98385af5ca46eff005d32
Marek Schubert <m.schubert@op.pl>
2023-11-21 20:37:41 +0100

    Image made nullable

--- Application/Common/Models/UserIdentityDto.cs
+++ Application/Common/Models/UserIdentityDto.cs
@@ -7,5 +7,5 @@ public class UserIdentityDto : IMappable<User>
 {
     public Guid Id { get; set; }
     public string Username { get; set; }
-    public ImageDto Image { get; set; }
+    public ImageDto? Image { get; set; }
 }
