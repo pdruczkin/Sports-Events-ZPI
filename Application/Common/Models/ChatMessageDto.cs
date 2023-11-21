effaf445928ac362d0ab0129033f11c42bf6cc29
NeverGiveUPek <pdruczkin@gmail.com>
2023-11-21 17:22:08 +0100

    Add ChatMessageDto, add sendMessage method, refactor

---
+++ Application/Common/Models/ChatMessageDto.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Mappings;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Common.Models;
+
+public class ChatMessageDto : IMappable<ChatMessage>
+{
+    public string Value { get; set; } = "";
+    public DateTime SentAtUtc { get; set; }
+    public string Username { get; set; } = "Unknown";
+    public Guid UserId { get; set; }
+    
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<ChatMessage, ChatMessageDto>()
+            .ForMember(x => x.Username, o => o.MapFrom(s => s.User.Username));
+    }
+    
+}
