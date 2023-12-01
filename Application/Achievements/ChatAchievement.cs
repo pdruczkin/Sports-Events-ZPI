3de54f3643ac4ab90a9ec6417cae22b80aa5207a
Marek Schubert <m.schubert@op.pl>
2023-12-01 11:23:55 +0100

    Added chat achievement

---
+++ Application/Achievements/ChatAchievement.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.ExtensionMethods;
+using Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Achievements;
+
+public class ChatAchievement
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public ChatAchievement(IApplicationDbContext dbContext, IDateTimeProvider dateTimeProvider)
+    {
+        _dbContext = dbContext;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task CheckChatAchievementAsync(Guid userId, CancellationToken cancellationToken)
+    {
+        var user = await _dbContext
+            .Users
+            .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+        if (user == null) throw new AppException("User not found");
+
+        await _dbContext.AddAchievementAsync(userId, "CHAT", _dateTimeProvider, cancellationToken);        
+    }
+}
---
+++ Infrastructure/Triggers/ChatAchievementTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Application.Achievements;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using EntityFrameworkCore.Triggered;
+using System;
+
+namespace Infrastructure.Triggers;
+
+public class ChatAchievementTrigger : IAfterSaveTrigger<ChatMessage>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public ChatAchievementTrigger(IApplicationDbContext dbContext, IDateTimeProvider dateTimeProvider)
+    {
+        _dbContext = dbContext;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task AfterSave(ITriggerContext<ChatMessage> context, CancellationToken cancellationToken)
+    {
+        if (context.ChangeType == ChangeType.Added)
+        {
+            var chatAchievementChecker = new ChatAchievement(_dbContext, _dateTimeProvider);
+            await chatAchievementChecker.CheckChatAchievementAsync(context.Entity.UserId, cancellationToken);
+        }
+    }
+}
